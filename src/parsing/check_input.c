7a2b0c5b676fb4600a05227a92ec6e815cf3305c
louisnfr <louis.raffin@gmail.com>
2022-01-24 11:57:20 +0100

    cub3D

--- src/parsing/check_input.c
+++ src/parsing/check_input.c
@@ -6,7 +6,7 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 15:31:38 by lraffin           #+#    #+#             */
-/*   Updated: 2022/01/21 17:39:45 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/01/24 11:20:25 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,23 @@
 
 static void	arg_error(void)
 {
-	printf("error: bad arguments\n");
+	printf("Error:\n bad arguments\n");
 	exit (EXIT_FAILURE);
 }
 
 static void	check_extension(char *s)
 {
-	if (open(s, O_RDONLY) < 0)
+	int fd;
+
+	fd = open(s, __O_DIRECTORY);
+	if (fd > 0)
+	{
+		close(fd);
 		arg_error();
-	if (!ft_strnstr(s, ".ber", ft_strlen(s)))
+	}
+	if (open(s, O_RDONLY | __O_NOFOLLOW) < 0)
+		arg_error();
+	if (!ft_strnstr(s, ".cub", ft_strlen(s)))
 		arg_error();
 }
 

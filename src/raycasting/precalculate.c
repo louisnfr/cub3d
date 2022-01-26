c42ae7d6a99a9a953320628132fbe05e2e119976
louisnfr <louis.raffin@gmail.com>
2022-01-26 14:45:16 +0100

    working on raycasting

--- src/raycasting/precalculate.c
+++ src/raycasting/precalculate.c
@@ -6,7 +6,7 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 18:26:00 by lraffin           #+#    #+#             */
-/*   Updated: 2022/01/26 02:01:49 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/01/26 14:44:20 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,15 +38,14 @@ int		precalculate_rays(t_data *data)
 		&data->mlx->length, &data->mlx->endian);
 
 	t_plane w1 = {1, 0, 0, 3};
-	t_plane w2 = {0, 1, 0, 3};
-	t_plane w3 = {0, 1, 0, -3};
-	t_plane w4 = {1, 0, 0, -3};
+	// t_plane w1 = {0, 1, 0, 3};
+	// t_plane w1 = {0, 1, 0, -3};
+	// t_plane w1 = {1, 0, 0, -3};
 
 	t_point cam = {0, 0, 0.5};
 	t_point line = {0, 0, 0};
 	t_point inter = {0, 0, 0};
 
-	double t = 1;
 
 	i = -1;
 	while (++i <= WIN_W)
@@ -57,9 +56,13 @@ int		precalculate_rays(t_data *data)
 			vect.x = ((i - WIN_W * 0.5) * rh);
 			vect.z = ((WIN_H * 0.5 - j) * rv);
 
+			double t = - ((w1.a * cam.x) + (w1.b * cam.y) + (w1.c * cam.z) + w1.d)
+					/ ((w1.a * vect.x) + (w1.b * vect.y) + (w1.c * vect.z));
+
 			line.x = cam.x + vect.x * t;
 			line.y = cam.y + vect.y * t;
 			line.z = cam.z + vect.z * t;
+
 
 			if (t > 0 && line.z >= 0 && line.z <= 1)
 				put_pixel(i, j, WHITE, data->mlx);

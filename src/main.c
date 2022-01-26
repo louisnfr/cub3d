44e307553f0e3d2f1f2a969f2b34735615a56630
louisnfr <louis.raffin@gmail.com>
2022-01-26 18:25:11 +0100

    working on raycasting

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/02 20:58:32 by vbachele          #+#    #+#             */
-/*   Updated: 2022/01/25 21:58:54 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/01/26 15:03:58 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,9 @@ int main(int ac, char **av)
 		return (EXIT_FAILURE);
 	if (!get_map(data, av[1]))
 		return (EXIT_FAILURE);
-	precalculate_rays(data);
+	// precalculate_rays(data);
 	init_controls(data);
+	mlx_loop_hook(data->mlx->ptr, precalculate_rays, data);
 	mlx_loop(data->mlx->ptr);
 	free_data(data);
 	return (EXIT_SUCCESS);
--- src/raycasting/precalculate.c
+++ src/raycasting/precalculate.c
@@ -6,7 +6,7 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 18:26:00 by lraffin           #+#    #+#             */
-/*   Updated: 2022/01/26 14:44:20 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/01/26 17:55:43 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 
 int		precalculate_rays(t_data *data)
 {
+	/*
+	int	matrix_z[3][3] = {
+		{cos(teta), - sin(teta), 0},
+		{sin(teta), cos(teta), 0},
+		{0, 0, 1}
+	};
+	*/
 	t_point	vect;
 
 	double	fov;
@@ -37,15 +44,17 @@ int		precalculate_rays(t_data *data)
 	data->mlx->buf1 = mlx_get_data_addr(data->mlx->img, &data->mlx->bpp,
 		&data->mlx->length, &data->mlx->endian);
 
-	t_plane w1 = {1, 0, 0, 3};
-	// t_plane w1 = {0, 1, 0, 3};
-	// t_plane w1 = {0, 1, 0, -3};
-	// t_plane w1 = {1, 0, 0, -3};
+	t_plane	w[4] =
+	{
+		{5, 0, 0, 3},
+		{0, 1, 0, 3},
+		{0, 1, 0, -3},
+		{5, 0, 0, -3}
+	};
 
 	t_point cam = {0, 0, 0.5};
 	t_point line = {0, 0, 0};
 	t_point inter = {0, 0, 0};
-
 
 	i = -1;
 	while (++i <= WIN_W)
@@ -53,19 +62,22 @@ int		precalculate_rays(t_data *data)
 		j = -1;
 		while (++j <= WIN_H)
 		{
-			vect.x = ((i - WIN_W * 0.5) * rh);
-			vect.z = ((WIN_H * 0.5 - j) * rv);
+			int k = -1;
+			while (++k < 4)
+			{
+				vect.x = ((i - WIN_W * 0.5) * rh);
+				vect.z = ((WIN_H * 0.5 - j) * rv);
 
-			double t = - ((w1.a * cam.x) + (w1.b * cam.y) + (w1.c * cam.z) + w1.d)
-					/ ((w1.a * vect.x) + (w1.b * vect.y) + (w1.c * vect.z));
+				double t = - ((w[k].a * cam.x) + (w[k].b * cam.y) + (w[k].c * cam.z) + w[k].d)
+						/ ((w[k].a * vect.x) + (w[k].b * vect.y) + (w[k].c * vect.z));
 
-			line.x = cam.x + vect.x * t;
-			line.y = cam.y + vect.y * t;
-			line.z = cam.z + vect.z * t;
+				line.x = cam.x + vect.x * t;
+				line.y = cam.y + vect.y * t;
+				line.z = cam.z + vect.z * t;
 
-
-			if (t > 0 && line.z >= 0 && line.z <= 1)
-				put_pixel(i, j, WHITE, data->mlx);
+				if (t > 0 && line.z >= 0 && line.z <= 1)
+					put_pixel(i, j, WHITE, data->mlx);
+			}
 		}
 	}
 

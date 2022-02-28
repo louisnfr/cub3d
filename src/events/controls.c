d3b4c38714bbe2d8deeca74ec67b3b34fe585962
louisnfr <louis.raffin@gmail.com>
2022-02-28 17:25:06 +0100

    mouse enhance

--- src/events/controls.c
+++ src/events/controls.c
@@ -6,7 +6,7 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 15:23:25 by lraffin           #+#    #+#             */
-/*   Updated: 2022/02/28 17:11:56 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/02/28 17:24:29 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,12 +66,13 @@ static int	mouse_move(int x, int y, t_data *data)
 	data->mouse->old_x = data->mouse->x;
 	data->mouse->x = x;
 	diff = ft_abs(data->mouse->old_x - x);
-	if (data->mouse->old_x < data->mouse->x)
+	if (diff > 2 && data->mouse->old_x < data->mouse->x)
 		mouse_orient_right(speed, data);
-	if (data->mouse->old_x > data->mouse->x)
+	if (diff > 2 && data->mouse->old_x > data->mouse->x)
 		mouse_orient_left(speed, data);
-	mlx_mouse_move(data->mlx->ptr, data->mlx->win,
-		(int)(WIN_W * 0.5), (int)(WIN_H * 0.5));
+	if (x < 40 || x > WIN_W - 40 || y < 40 || y > WIN_H - 40)
+		mlx_mouse_move(data->mlx->ptr, data->mlx->win,
+			(int)(WIN_W * 0.5), (int)(WIN_H * 0.5));
 	return (SUCCESS);
 }
 

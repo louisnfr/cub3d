7da59129728a3a803f6ae0b6ed5dd4b03fe465f8
louisnfr <louis.raffin@gmail.com>
2022-02-28 17:12:24 +0100

    mouse enghance

--- src/events/controls.c
+++ src/events/controls.c
@@ -6,7 +6,7 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 15:23:25 by lraffin           #+#    #+#             */
-/*   Updated: 2022/02/28 15:40:14 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/02/28 17:11:56 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,16 +58,20 @@ static int	key_release(int key, t_data *data)
 
 static int	mouse_move(int x, int y, t_data *data)
 {
+	int		diff;
 	double	speed;
 
-	speed = 0.03;
+	(void)y;
+	speed = 0.02;
 	data->mouse->old_x = data->mouse->x;
 	data->mouse->x = x;
+	diff = ft_abs(data->mouse->old_x - x);
 	if (data->mouse->old_x < data->mouse->x)
 		mouse_orient_right(speed, data);
 	if (data->mouse->old_x > data->mouse->x)
 		mouse_orient_left(speed, data);
-	mlx_mouse_move(data->mlx->ptr, data->mlx->win, (int)(WIN_W * 0.5), y);
+	mlx_mouse_move(data->mlx->ptr, data->mlx->win,
+		(int)(WIN_W * 0.5), (int)(WIN_H * 0.5));
 	return (SUCCESS);
 }
 
